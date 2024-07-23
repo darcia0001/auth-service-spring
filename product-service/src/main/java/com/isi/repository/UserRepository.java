a871cf540392e76f54e91fa6a7574e766cad7475
darcia <darcia0001@gmail.com>
2024-07-23 18:31:44 +0000

    add filter

--- product-service/src/main/java/com/isi/repository/UserRepository.java
+++ product-service/src/main/java/com/isi/repository/UserRepository.java
@@ -3,8 +3,12 @@ package com.isi.repository;
 import com.isi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
+   // User findByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 
 }
--- product-service/src/main/java/com/isi/service/UserServiceImpl.java
+++ product-service/src/main/java/com/isi/service/UserServiceImpl.java
@@ -1,26 +1,50 @@
 package com.isi.service;
 
+import com.isi.controller.ResourceNotFoundException;
 import com.isi.entity.User;
-import com.isi.service.UserService;
 import com.isi.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
 
 @Service
 @AllArgsConstructor
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService , UserDetailsService {
 
     @Autowired
     private final UserRepository repository = null;
 
     @Override
     public User getUserByEmail(String email) {
-        return Optional.ofNullable( repository.findByEmail(email))
-                .orElseThrow(() -> new RuntimeException("User with email is not found."));
+        return  repository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("User with email is not found."));
 
+    }
+
+    public UserDetails loadUserByUsername(String username) throws ResourceNotFoundException {
+        User user = repository.findByEmail(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), getAuthorities(user));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(User user) {
+       List<String> list= new ArrayList<>() ;
+       list.add(user.getRole());
+        return list.stream()
+                .map(role -> new SimpleGrantedAuthority(role))
+                .collect(Collectors.toList());
     }
 
     public void save(User user){
